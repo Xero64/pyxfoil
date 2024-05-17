c1bfa96377e5df43a4f26d16d5de59973d527c66
Xero64 <37905073+Xero64@users.noreply.github.com>
2024-05-17 16:19:19 +0200

    Added pyxfoil_result_plots.py.

---
+++ scripts/pyxfoil_result_plots.py
@@ -0,0 +1,64 @@
+#%%
+# Import Dependencies
+from pyxfoil import Xfoil, set_workdir#, set_xfoilexe
+set_workdir('C:/Xfoil_WIP') # Sets the working directory for pyxfoil.
+# set_xfoilexe('C:/Xfoil-6.99/xfoil.exe') # Sets the path of the xfoil executable.
+
+#%%
+# Creates Xfoil object from reading dat file.
+xfoil = Xfoil('NACA 0012')
+xfoil.points_from_dat('../files/NACA_0012_180.dat')
+xfoil.set_ppar(180)
+
+#%%
+# Runs xfoil for the following parameters
+al = 0.0
+mach = 0.0
+re = 100000.0
+rescase = xfoil.run_result(al, mach=mach, re=re)
+
+#%%
+# Plots xfoil airfoil profile
+ax1 = xfoil.plot_profile(ls='-')
+
+#%%
+# Shows plots for cases in xfoil cases
+ax2 = None
+for result in xfoil.results.values():
+    ax2 = result.plot_result(yaxis='cp', ax=ax2, ls='-x')
+_ = ax2.legend()
+
+#%%
+# Shows plots for cases in xfoil cases
+ax3 = None
+for result in xfoil.results.values():
+    ax3 = result.plot_result(yaxis='ds', ax=ax3, ls='-o')
+_ = ax3.legend()
+
+#%%
+# Shows plots for cases in xfoil cases
+ax4 = None
+for result in xfoil.results.values():
+    ax4 = result.plot_result(yaxis='th', ax=ax4, ls='-o')
+_ = ax4.legend()
+
+#%%
+# Shows plots for cases in xfoil cases
+ax5 = None
+for result in xfoil.results.values():
+    ax5 = result.plot_result(yaxis='cf', ax=ax5, ls='-o')
+_ = ax5.legend()
+
+#%%
+# Shows plots for cases in xfoil cases
+ax6 = None
+for result in xfoil.results.values():
+    ax6 = result.plot_result(yaxis='h', ax=ax6, ls='-o')
+_ = ax6.legend()
+
+#%%
+# Shows plots for cases in xfoil cases
+ax7 = None
+for result in xfoil.results.values():
+    ax7 = result.plot_result(yaxis='ue', ax=ax7, ls='-o')
+_ = ax7.legend()
